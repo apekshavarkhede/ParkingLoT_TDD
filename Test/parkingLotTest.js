9a1b35a3adc6bf93d55ed4eb82df1954316ac04d
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-04-27 23:49:45 +0530

    Add: call the parkCar function

---
+++ Test/parkingLotTest.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert
+
+describe('Testing for parkinLot', function () {
+    it('given car when park should return park ', function () {
+        let car = {}
+        parkingLot.parkCar(car)
+    })
+})
