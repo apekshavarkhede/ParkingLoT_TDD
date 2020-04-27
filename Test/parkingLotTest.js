4a5f88c1b62971a20656c7272a004081acb7eba3
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-04-28 01:01:07 +0530

    Refactored: move creation of parkingLot object in beforeEach

--- Test/parkingLotTest.js
+++ Test/parkingLotTest.js
@@ -2,16 +2,20 @@ var assert = require('chai').assert
 var parkingLot = require('../main/parkingLot')
 
 describe('Testing for parkinLot', function () {
+
+    let parkingLotObject;
+    beforeEach(function () {
+        parkingLotObject = new parkingLot();
+    })
+
     it('given car when park should return park ', function () {
-        let parkingLotObject = new parkingLot();
         let car = {}
         let parkTheCar = parkingLotObject.parkCar(car)
         assert.isTrue(parkTheCar)
     })
 
-    it.only('given car as other than object should throw exception', function () {
+    it('given car as other than object should throw exception', function () {
         try {
-            let parkingLotObject = new parkingLot();
             let car = 0;
         } catch (error) {
             let parkTheCar = parkingLotObject.parkCar(car)
