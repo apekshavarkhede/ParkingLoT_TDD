cd0a17d343972456829c69dd1de7a9aa85e7c2c1
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-04-28 19:19:55 +0530

    Refactor:  move stub in beforeEach

--- Test/parkingLotTest.js
+++ Test/parkingLotTest.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert
 var parkingLot = require('../main/parkingLot')
 var owner = require('../main/owner')
-var sinon = require('sinon')
+var sinon = require('./node_modules/sinon')
 var expect = require('chai').expect
 
 describe('Testing for parkinLot', function () {
@@ -9,23 +9,25 @@ describe('Testing for parkinLot', function () {
     let parkingLotObject;
     beforeEach(function () {
         parkingLotObject = new parkingLot();
+        sinon.stub(parkingLotObject, "checkParkingLotFull").
+            onFirstCall().returns(false).onSecondCall().returns(true)
+    })
+
+    this.afterEach(function () {
+        parkingLotObject.checkParkingLotFull.restore()
     })
 
     //UC1...park the car
     it('given car when park should return park ', function () {
         let car = {}
-        let stub = sinon.stub(parkingLotObject, "checkParkingLotFull").returns(false);
         expect(parkingLotObject.parkCar(car)).to.be.equal(true)
-        stub.restore()
     })
 
     // throw exception when  car is not object typed
     it('given car as other than object should throw exception', function () {
         try {
             let car = 0;
-            let stub = sinon.stub(parkingLotObject, "checkParkingLotFull").returns(false);
             let parkTheCar = parkingLotObject.parkCar(car)
-            stub.restore()
         } catch (error) {
             assert.equal(error.message, "car must be an object")
         }
@@ -34,11 +36,9 @@ describe('Testing for parkinLot', function () {
     //UC2..unpark the car
     it('given parked car when when unpark return true', function () {
         let car = {};
-        let stub = sinon.stub(parkingLotObject, "checkParkingLotFull").returns(false);
         let parkCar = parkingLotObject.parkCar(car)
         let unParkCar = parkingLotObject.unParkCar(car)
         assert.isTrue(unParkCar)
-        stub.restore()
     })
 
     // throw exception when unpark the car which is not park
@@ -46,10 +46,8 @@ describe('Testing for parkinLot', function () {
         try {
             let car = {};
             let car1 = {};
-            let stub = sinon.stub(parkingLotObject, "checkParkingLotFull").returns(false);
             let parkCar = parkingLotObject.parkCar(car)
             let unParkCar = parkingLotObject.unParkCar(car1)
-            stub.restore()
         } catch (error) {
             assert.equal(error.message, "car not parked");
         }
@@ -59,23 +57,17 @@ describe('Testing for parkinLot', function () {
     it('given parking lot when is full then inform owner', function () {
         let car = {}
         let car1 = {}
-        let stub = sinon.stub(parkingLotObject, "checkParkingLotFull").
-            onFirstCall().returns(false).onSecondCall().returns(true)
         expect(parkingLotObject.parkCar(car)).to.be.equal(true)
         expect(parkingLotObject.parkCar(car1)).to.be.equal("Parking lot full")
-        stub.restore()
     })
 
-     // check is owner receive parking lot full notification
-     it.only('given parking lot full when check is owner receive notification should return true', function () {
+    // check is owner receive parking lot full notification
+    it('given parking lot full when check is owner receive notification should return true', function () {
         let car = {};
         let car1 = {};
-        let stub = sinon.stub(parkingLotObject, "checkParkingLotFull").
-            onFirstCall().returns(false).onSecondCall().returns(true)
         expect(parkingLotObject.parkCar(car)).to.be.equal(true)
         parkingLotObject.parkCar(car1)
         expect(owner.informParkingLotFull()).to.be.equal(true)
-        stub.restore()
     })
 
 })
